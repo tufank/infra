ca151ce86771bfdf9e77cf4369209eb29708ac51
Tufan Karadere <tufank@gmail.com>
2025-01-22 20:32:46 +0100

    Add terraform-libvirt-postgresql module

---
+++ iac/modules/terraform-libvirt-postgresql/nodes.tf
@@ -0,0 +1,16 @@
+module "nodes" {
+  for_each            = var.node_names
+  source              = "../terraform-libvirt-node"
+  hostname            = each.key
+  vcpu                = var.vcpu
+  memory              = var.memory
+  lmi                 = var.lmi
+  ssh_user            = var.ssh_user
+  timezone            = var.timezone
+  domain_name         = var.domain_name
+  ssh_authorized_keys = var.ssh_authorized_keys
+}
+
+output "node_names_and_ips" {
+  value = { for k, v in module.nodes : v.name => v.ip }
+}
